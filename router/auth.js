0f7c65b5615fa743f59cb0fa4362d9376938518e
AnUj <shakya.anuj910@gmail.com>
2023-06-05 02:15:03 +0530

    domain issue

--- router/auth.js
+++ router/auth.js
@@ -108,7 +108,7 @@ router.post("/login", async (req, res) => {
         // expires: new Date(Date.now() + 25892000000),
         httpOnly: true,
         sameSite: "none",
-        secure: true,
+        // secure: true,
       });
 
       // res.use(cookieSession({
